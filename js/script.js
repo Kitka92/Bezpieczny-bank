714cb924bc41313ecb58d0ae2033f4cc826c4a6b
Anita Swieconek <anita.swieconek92@gmail.com>
2017-03-05 12:01:33 +0100

    hamburger menu functions

--- js/script.js
+++ js/script.js
@@ -1,14 +1,13 @@
 $(document).ready(function(){
   
-  $('#nav-icon3').click(function(){
-		$(this).toggleClass('open');
-	});
+  
   
   var hamburgerMenu = $('#nav-icon3');
   var nav = $('nav');
   var list = nav.find('ul');
   hamburgerMenu.addClass('hidden');
   
+  /*
   $(window).on('resize', function() {
     if ($(window).width() <= 882) {
       hamburgerMenu.removeClass('hidden');
@@ -18,8 +17,19 @@ $(document).ready(function(){
       nav.show();
     }
   });
+  */
+  
+  if ($(window).width() <= 882) {
+    hamburgerMenu.removeClass('hidden');
+    nav.hide();
+  } else {
+    hamburgerMenu.addClass('hidden');
+    nav.show();
+  }
   
   hamburgerMenu.on('click', function() {
+    
+    $(this).toggleClass('open');
     
     if (nav.is(':hidden')) {
       nav.show();
